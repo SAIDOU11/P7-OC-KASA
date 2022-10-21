e9ce51e9e3965618fb45690f95b53e3b9ae79d9b
Salif Dieye Saidou <104588234+SAIDOU11@users.noreply.github.com>
2022-10-21 17:39:12 +0200

    page not found query mobile

--- kasa/src/pages/NotFound.jsx
+++ kasa/src/pages/NotFound.jsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 export function NotFound() {
   return (
     <div className="notfound">
-      <div className="container">
-        <h1 className="error">404</h1>
-        <p className="paragraph">
+      <div className="notfound__container">
+        <h1 className="notfound__error">404</h1>
+        <p className="notfound__paragraph">
           Oups! La page que vous demandez n'existe pas.
         </p>
-        <Link to="/" className="link">
+        <Link to="/" className="notfound__link">
           Retourner sur la page d'accueil
         </Link>
       </div>
