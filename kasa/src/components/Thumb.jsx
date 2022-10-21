c8c44a854a53dd7d032caf3cd313be016c9cf68f
Salif Dieye Saidou <104588234+SAIDOU11@users.noreply.github.com>
2022-10-21 15:31:53 +0200

    refactoring

--- kasa/src/components/Thumb.jsx
+++ kasa/src/components/Thumb.jsx
@@ -1,9 +1,9 @@
 export default function Thumb({ cover, title }) {
   return (
     <div className="thumb">
-      <img src={cover} alt="cover product" className="image" />
-      <div className="covert"></div>
-      <h2 className="title">{title}</h2>
+      <img src={cover} alt="cover product" className="thumb__image" />
+      <div className="thumb__covert"></div>
+      <h2 className="thumb__title">{title}</h2>
     </div>
   )
 }
