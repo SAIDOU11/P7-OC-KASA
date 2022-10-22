97ffa43a3bed012240aa65ee529f0dafeb7cc2b7
Salif Dieye Saidou <104588234+SAIDOU11@users.noreply.github.com>
2022-10-22 16:21:14 +0200

    slider image

--- kasa/src/components/ImageSlider.jsx
+++ kasa/src/components/ImageSlider.jsx
@@ -1,7 +1,18 @@
 import { useState } from 'react'
+import LeftArrow from '../images/LeftArrow.png'
+import RightArrow from '../images/RightArrow.png'
 
 export function ImageSlider({ slides }) {
   const [currentIndex, setCurrentIndex] = useState(0)
+  const length = slides.length
+
+  const goToPrevious = () => {
+    setCurrentIndex(currentIndex === 0 ? length - 1 : currentIndex - 1)
+  }
+
+  const goToNext = () => {
+    setCurrentIndex(currentIndex === length - 1 ? 0 : currentIndex + 1)
+  }
 
   return (
     <div className="ImageSlider">
@@ -9,14 +20,28 @@ export function ImageSlider({ slides }) {
         return (
           <div
             key={index}
-            className={index === currentIndex ? 'StyleSlide ' : ' StyleSlide'}
+            className={
+              index === currentIndex
+                ? 'ImageSlider__Active '
+                : ' ImageSlider__Inactive'
+            }
           >
             {index === currentIndex && (
-              <img src={picture} alt="" className="picture" />
+              <img src={picture} alt="" className="ImageSlider__picture" />
             )}
           </div>
         )
       })}
+      {length > 1 ? (
+        <>
+          <div className="previousStyle" onClick={goToPrevious}>
+            <img src={LeftArrow} alt="" className="previousStyle__previous" />
+          </div>
+          <div className="nextStyle" onClick={goToNext}>
+            <img src={RightArrow} alt="" className="nextStyle__next" />
+          </div>
+        </>
+      ) : null}
     </div>
   )
 }
