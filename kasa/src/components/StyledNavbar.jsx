80dfaebdf80b190e13b0bb11311c83cb06a33f9d
Salif Dieye Saidou <104588234+SAIDOU11@users.noreply.github.com>
2022-10-30 14:35:13 +0100

    test

--- kasa/src/components/StyledNavbar.jsx
+++ kasa/src/components/StyledNavbar.jsx
@@ -1,19 +1,19 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import LOGO from '../images/LOGO.png'
 
 export default function StyledNavbar() {
   return (
     <nav className="navbar">
-      <Link to="/">
+      <NavLink to="/">
         <img src={LOGO} alt="Logo kasa" className="navbar__logo" />
-      </Link>
+      </NavLink>
       <div className="navbar__link">
-        <Link to="/" end className="navbar__link">
+        <NavLink to="/" end className="navbar__link">
           Accueil
-        </Link>
-        <Link to="/about" className="navbar__link">
+        </NavLink>
+        <NavLink to="/about" className="navbar__link">
           A Propos
-        </Link>
+        </NavLink>
       </div>
     </nav>
   )
